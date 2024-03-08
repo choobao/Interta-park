ca0ef6c08597280987d629f19eb911fa720173fa
choobao <154485888+choobao@users.noreply.github.com>
2024-03-08 13:17:08 +0900

    Feat: user api 작성 중

--- src/user/dto/create.dto.ts
+++ src/user/dto/create.dto.ts
@@ -1,12 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Min } from 'class-validator';
 
 export class CreateDto {
-  @IsEmail()
+  @IsEmail({}, { message: '유효한 이메일을 입력해주세요.' })
   @IsNotEmpty({ message: '이메일을 입력해주세요.' })
   email: string;
 
   @IsString()
   @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
+  @Min(6, { message: '비밀번호는 최소 6자 이상입니다.' })
   password: string;
 
   @IsString()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Point } from './entities/point.entity';
+import { compare, hash, hashSync } from 'bcrypt';
+import _ from 'lodash';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    @InjectRepository(Point)
+    private readonly userRepository: Repository<User>,
+    private readonly pointRepository: Repository<Point>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  //회원가입
+  async register(email: string, password: string, name: string) {
+    const existingUser = await this.findByEmail(email);
+    if (existingUser) {
+      throw new ConflictException(
+        '이미 해당 이메일로 가입된 사용자가 있습니다.',
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+    await this.userRepository.save({
+      email,
+      password: hashedPassword,
+      name,
+    });
+  }
+
+  //로그인
+  async login(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (_.isNil(user)) {
+      throw new UnauthorizedException('이메일을 확인해주세요.');
+    }
+
+    if (!(await compare(password, user.password))) {
+      throw new UnauthorizedException('비밀번호를 확인해주세요.');
+    }
+
+    const payload = { email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  //공연관계자 등록
+
+  //내 정보 조회
+
+  async findByEmail(email: string) {
+    return await this.userRepository.findOneBy({ email });
+  }
+}
